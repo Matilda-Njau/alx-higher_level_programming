939d9a32394c295dd9842808d5426519b683529b
Matilda-Njau <matildawanjirunjau@gmail.com>
2024-03-21 23:58:32 -0700

    Program that imports functions from the file calculator_1.py, does some Maths, and prints the result.

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import calculator_1 as calculator
+
+a = 10
+b = 5
+result1 = calculator.add(10, 5)
+result2 = calculator.sub(10, 5)
+result3 = calculator.mul(10, 5)
+result4 = calculator.div(10, 5)
+print(f"10 + 5 = {result1}")
+print(f"10 - 5 = {result2}")
+print(f"10 * 5 = {result3}")
+print(f"10 / 5 = {result4}")
